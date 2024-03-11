9fdd8e2387995d306a5ceb759b5edad9167cdb5c
mjjstockman <mjjstockman@gmail.com>
2024-03-11 13:34:26 +0000

    add dev preset to morgan use

--- server/db/connection.js
+++ server/db/connection.js
@@ -11,15 +11,8 @@ const client = new MongoClient(uri, {
 });
 
 try {
-  // Connect the client to the server
   await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db('admin').command({ ping: 1 });
-  console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  console.log('Connected to MongoDB successfully!');
 } catch (err) {
-  console.error(err);
+  console.error('Error connecting to MongoDB:', err);
 }
-
-let db = client.db('employees');
-
-export default db;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import createError from 'http-errors';
+// import { User } from './models/user';
+// import { v4 as uuidv4 } from 'uuid';
+
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
+
 app.use(express.json());
 
 // start the Express server
