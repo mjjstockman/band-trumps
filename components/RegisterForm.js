4a5cab057ce8191440d7a969e25985ae3df87be0
mjjstockman <mjjstockman@gmail.com>
2024-03-14 14:41:58 +0000

    add username field to RegisterForm

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -4,16 +4,6 @@ const RegisterForm = () => {
   return (
     <section className='bg-gray-50 dark:bg-gray-900'>
       <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
-        <a
-          href='#'
-          className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'>
-          <img
-            className='w-8 h-8 mr-2'
-            src='https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg'
-            alt='logo'
-          />
-          Flowbite
-        </a>
         <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
           <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
             <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
@@ -32,6 +22,21 @@ const RegisterForm = () => {
                   id='email'
                   className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                   placeholder='name@company.com'
+                  required=''
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor='username'
+                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                  Username
+                </label>
+                <input
+                  type='input'
+                  name='username'
+                  id='username'
+                  placeholder='username'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                   required=''
                 />
               </div>
