faa11b400ca1d08d3e76daa5066ae5d6fdeabfc4
mjjstockman <mjjstockman@gmail.com>
2024-04-29 14:08:25 +0100

    setup nodejs, nodemon, ts-node, express and typescript

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 4813;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
