e81aed9bdd6461bb5ace209e9417130d7cd82e25
mjjstockman <mjjstockman@gmail.com>
2024-04-30 12:57:28 +0100

    use cleanEnv for DB connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,21 @@
+import 'dotenv/config';
+import env from './util/validateEnv';
+import mongoose from 'mongoose';
 import express from 'express';
 const app = express();
-const port = 4813;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGODB_URI)
+  .then(() => {
+    console.log('Mongoose connected!!');
+    app.listen(port, () => {
+      console.log(`Server running on port: ${port}`);
+    });
+  })
+  .catch(console.error);
