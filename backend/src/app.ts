f91c739356c67e2cc093d419d2719a327cce069a
mjjstockman <mjjstockman@gmail.com>
2024-04-30 14:42:17 +0100

    add initial card model

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import CardModel from './models/card';
 
 const app = express();
 
---
+++ backend/src/models/card.ts
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+import { InferSchemaType, model, Schema } from 'mongoose';
+
+const cardSchema = new Schema(
+  {
+    band: { type: Schema.Types.ObjectId, ref: 'Band', required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    gigsAttended: [{ type: Schema.Types.ObjectId, ref: 'Gig' }],
+    numberOfGigs: Number,
+    merchandisePurchased: Number,
+    spotifyStreams: Number,
+  },
+  { timestamps: true }
+);
+
+type Card = InferSchemaType<typeof cardSchema>;
+
+export default model<Card>('Card', cardSchema);
