510fb4e50c249466d0b949cea3681c445f009a6c
mjjstockman <mjjstockman@gmail.com>
2024-05-01 13:45:41 +0100

    add basic express error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,24 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import CardModel from './models/card';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/', async (req, res, next) => {
+  //   res.send('Hello, World!');
+  try {
+    // throw Error('An error!');
+    const cards = await CardModel.find().exec();
+    res.status(200).json(cards);
+  } catch (error) {
+    next(error);
+  }
 });
 
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = 'An unknown error occurred';
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
+});
 export default app;
